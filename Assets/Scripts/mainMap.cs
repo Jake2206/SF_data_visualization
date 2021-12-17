7e809628d3eb5be12227fe5fdb5ae9febc878d5f
Jake2206 <jla2206@columbia.edu>
2021-12-17 08:37:52 -0500

    Base map choropleth

--- Assets/Scripts/mainMap.cs
+++ Assets/Scripts/mainMap.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class mainMap : MonoBehaviour
 {
@@ -9,15 +10,31 @@ public class mainMap : MonoBehaviour
     public GameObject combinedMap;
     public GameObject mainSfMap;
     public GameObject mainEmsMap;
+    public GameObject mainBaseMap;
+    public TextMeshProUGUI mainText;
+    private string defaultText;
+    private string altText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainEmsMap.SetActive(false);
+        defaultText = "Aggregated and Normalized Data covering 2011-2016";
+        altText = "Aggregated and Normalized Stop and Frisk Data 2011-2016";
     }
 
     public void setMaps()
     {
+        if (sfTabMap.activeInHierarchy && emsTabMap.activeInHierarchy)
+        {
+            mainBaseMap.SetActive(true);
+            mainText.text = altText;
+        }
+        else
+        {
+            mainBaseMap.SetActive(false);
+            mainText.text = defaultText;
+        }
         if(sfTabMap.activeInHierarchy || emsTabMap.activeInHierarchy)
         {
             combinedMap.SetActive(false);
